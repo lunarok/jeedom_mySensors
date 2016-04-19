35098c8fcb9b2f7ca0731037e1b6c94e21b3bd8f
Azema <manuel.hervo@gmail.com>
2016-04-19 19:53:34 +0200

    Correction des balises HTML

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -52,53 +52,50 @@ if (!isConnect()) {
             Ajouter un mySensors réseau
           </a>
           <table id="table_net" class="table table-bordered table-condensed">
-              <tbody>
-                <?php
-
-                if (config::byKey('netgate','mySensors') != '') {
-                  $net = explode(";", config::byKey('netgate','mySensors'));
-                  foreach ($net as $value) {
-                    echo "<tr><td><input name='network' type='text' class='input_network' placeholder='ip:port' value='" . $value . "'></td><td><i class='fa fa-minus-circle cursor'></i></td></tr>";
-                  }
+            <tbody>
+              <?php
+              if (config::byKey('netgate','mySensors') != '') {
+                $net = explode(";", config::byKey('netgate','mySensors'));
+                foreach ($net as $value) {
+                  echo "<tr><td><input name='network' type='text' class='input_network' placeholder='ip:port' value='" . $value . "'></td><td><i class='fa fa-minus-circle cursor'></i></td></tr>";
                 }
-
-                 ?>
-              </tbody>
+              }
+              ?>
+            </tbody>
           </table>
-
+        </div>
+      </div>
+    </fieldset>
+  </div>
+</form>
+<?php
+if (config::byKey('jeeNetwork::mode') == 'master') {
+  foreach (jeeNetwork::byPlugin('mySensors') as $jeeNetwork) {
+    ?>
+    <form class="form-horizontal slaveConfig" data-slave_id="<?php echo $jeeNetwork->getId(); ?>">
+      <fieldset>
+        <div class="form-group">
+          <label class="col-lg-4 control-label">{{Gateway série esclave}} <?php echo $jeeNetwork->getName() ?></label>
+          <div class="col-lg-4">
+            <select class="slaveConfigKey form-control" data-l1key="nodeGateway">
+              <option value="none">{{Aucune}}</option>
+              <?php
+              foreach ($jeeNetwork->sendRawRequest('jeedom::getUsbMapping', array('gpio' => true)) as $name => $value) {
+                echo '<option value="' . $name . '">' . $name . ' (' . $value . ')</option>';
+              }
+              ?>
+            </select>
           </div>
         </div>
 
-    </fieldset>
-  </form>
-  <?php
-  if (config::byKey('jeeNetwork::mode') == 'master') {
-    foreach (jeeNetwork::byPlugin('mySensors') as $jeeNetwork) {
-      ?>
-      <form class="form-horizontal slaveConfig" data-slave_id="<?php echo $jeeNetwork->getId(); ?>">
-        <fieldset>
-          <div class="form-group">
-            <label class="col-lg-4 control-label">{{Gateway série esclave}} <?php echo $jeeNetwork->getName() ?></label>
-            <div class="col-lg-4">
-              <select class="slaveConfigKey form-control" data-l1key="nodeGateway">
-                <option value="none">{{Aucune}}</option>
-                <?php
-                foreach ($jeeNetwork->sendRawRequest('jeedom::getUsbMapping', array('gpio' => true)) as $name => $value) {
-                  echo '<option value="' . $name . '">' . $name . ' (' . $value . ')</option>';
-                }
-                ?>
-              </select>
-            </div>
-          </div>
-
-        </fieldset>
-      </form>
-      <?php
-    }
+      </fieldset>
+    </form>
+    <?php
   }
-  ?>
+}
+?>
 
-  <script>
+<script>
 
   $('.bt_network').on('click',function(){
     var newInput = $("<tr><td><input name='network' type='text' class='input_network' placeholder='ip:port'></td><td><i class='fa fa-minus-circle cursor'></i></td></tr>");
@@ -110,34 +107,31 @@ if (!isConnect()) {
   });
 
   function mySensors_postSaveConfiguration(){
-  var network = '';
-  $('.input_network').each(function(index, value) {
-    if (network != '' ) {
-      network = network + ';' + $(this).value();
-    } else {
-      network = $(this).value();
-    }
-  });
-  $.ajax({// fonction permettant de faire de l'ajax
+    var network = '';
+    $('.input_network').each(function(index, value) {
+      if (network != '' ) {
+        network = network + ';' + $(this).value();
+      } else {
+        network = $(this).value();
+      }
+    });
+    $.ajax({// fonction permettant de faire de l'ajax
       type: "POST", // methode de transmission des données au fichier php
       url: "plugins/mySensors/core/ajax/mySensors.ajax.php", // url du fichier php
       data: {
-          action: "netgate",
-          value: network,
+        action: "netgate",
+        value: network,
       },
       dataType: 'json',
       error: function (request, status, error) {
-          handleAjaxError(request, status, error);
+        handleAjaxError(request, status, error);
       },
       success: function (data) { // si l'appel a bien fonctionné
-  if (data.state != 'ok') {
-    $('#div_alert').showAlert({message: data.result, level: 'danger'});
-    return;
+        if (data.state != 'ok') {
+          $('#div_alert').showAlert({message: data.result, level: 'danger'});
+          return;
+        }
+      }
+    });
   }
-  }
-  });
-  }
-  </script>
-</div>
-</fieldset>
-</form>
+</script>
