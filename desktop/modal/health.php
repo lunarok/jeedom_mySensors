b8da761fbd4494fe6808b01407260482ade8b320
Pierre Wieser <pwieser@trychlos.org>
2019-05-21 21:58:57 +0200

    Remove battery indicator if no level nor characteristics are specified

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -46,7 +46,12 @@ foreach ($eqLogics as $eqLogic) {
 	echo '<td>' . $status . '</td>';
 	$battery_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
 	$battery = $eqLogic->getStatus('battery'); 
+	$battery_type = $eqLogic->getConfiguration('battery_type', '');
 	if ($battery == '') {
+		$battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fas fa-plug"></i></span>';
+  } elseif ($battery == 0 && $battery_type == '') {
+		$eqLogic->setStatus( 'battery', '' );
+		$eqLogic->setStatus( 'batteryDatetime', '' );
 		$battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fas fa-plug"></i></span>';
   } elseif ($battery < 20) {
 		$battery_status = '<span class="label label-danger" style="font-size : 1em;">' . $battery . '%</span>';
