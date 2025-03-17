23c8dfdf18b672f62d1de4a281706df8ee459a4c
Pierre Wieser <p.wieser@trychlos.org>
2025-03-17 14:51:41 +0100

    Improve gateway logging

--- resources/mysensors.js
+++ resources/mysensors.js
@@ -45,6 +45,30 @@ const I_SKETCH_NAME					= 11;
 const I_SKETCH_VERSION				= 12;
 const I_REBOOT						= 13;
 
+function logDebug(msg) {
+	if (log == 'debug') {
+		console.debug((new Date()) + " [DEBUG] " + msg);
+	}
+}
+
+function logInfo(msg) {
+	if (log == 'debug' || log == 'info') {
+		console.log((new Date()) + " [INFO] " + msg);
+	}
+}
+
+function logWarning(msg) {
+	if (log == 'debug' || log == 'info' || log == 'warning') {
+		console.log((new Date()) + " [WARNING] " + msg);
+	}
+}
+
+function logError(msg) {
+	if (log == 'debug' || log == 'info' || log == 'warning' || log == 'error') {
+		console.log((new Date()) + " [ERROR] " + msg);
+	}
+}
+
 function encode(destination, sensor, command, acknowledge, type, payload) {
 	var msg = destination.toString(10) + ";" + sensor.toString(10) + ";" + command.toString(10) + ";" + acknowledge.toString(10) + ";" + type.toString(10) + ";";
 	if (command == 4) {
@@ -62,12 +86,12 @@ function encode(destination, sensor, command, acknowledge, type, payload) {
 
 function connectJeedom(messagetype, sender, sensor, type, payload) {
 	jeeApi = urlJeedom + "&messagetype="+messagetype.toString()+"&sender="+sender.toString()+"&sensor=" + sensor.toString() +"&type=" + type.toString() + "&payload="+payload;
-	if (log == 'debug') {console.log((new Date()) + " : " + jeeApi);}
+	logDebug(jeeApi);
 	request(jeeApi, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			//if (log == 'debug') {console.log((new Date()) + " - Return OK from Jeedom");}
-		}else{
-			console.log((new Date()).toLocaleString(), error);
+			logDebug("Return OK from Jeedom");
+		} else{
+			logError("connectJeedom error: "+error+", statusCode: "+response.statusCode);
 		}
 	});
 }
@@ -75,13 +99,13 @@ function connectJeedom(messagetype, sender, sensor, type, payload) {
 function sendTime(destination, sensor, gw) {
 	var payload = new Date().getTime()/1000;
 	var td = encode(destination, sensor, C_INTERNAL, "0", I_TIME, payload);
-	console.log('-> ' + td.toString());
+	logInfo('-> ' + td.toString());
 	gw.write(td);
 }
 
 function sendConfig(destination, gw) {
 	var td = encode(destination, NODE_SENSOR_ID, C_INTERNAL, "0", I_CONFIG, "M");
-	console.log('-> ' + td.toString());
+	logInfo('-> ' + td.toString());
 	gw.write(td);
 }
 
@@ -92,8 +116,9 @@ function appendData(str, gw) {
         pos++;
     }
     if (str.charAt(pos) == '\n') {
-        rfReceived(appendedString.trim(), gw);
-				console.log((new Date()) + " : " + appendedString.trim());
+		const s = appendedString.trim();
+		logInfo('<- ' + s);
+        rfReceived(s, gw);
         appendedString="";
     }
     if (pos < str.length) {
@@ -103,7 +128,6 @@ function appendData(str, gw) {
 
 function rfReceived(data,gw) {
 	if ((data != null) && (data != "")) {
-		//LogDate("debug", "-> "  + td.toString() );
 		// decoding message
 		var datas = data.toString().split(";");
 		var sender = +datas[0];
@@ -126,79 +150,79 @@ function rfReceived(data,gw) {
 		// decision on appropriate response
 		switch (command) {
 			case C_PRESENTATION:
-			if (sensor == NODE_SENSOR_ID)
-			//	saveProtocol(sender, payload); //arduino ou arduino relay
-			;
-			else
-			connectJeedom('saveSensor', sender, sensor, type, payload);
-			//connectJeedom('saveLibVersion', sender, sensor, type, payload);
-			break;
+				if (sensor == NODE_SENSOR_ID)
+					//	saveProtocol(sender, payload); //arduino ou arduino relay
+					;
+				else
+					connectJeedom('saveSensor', sender, sensor, type, payload);
+				//connectJeedom('saveLibVersion', sender, sensor, type, payload);
+				break;
 			case C_SET:
-			connectJeedom('saveValue', sender, sensor, type, payload);
-			break;
+				connectJeedom('saveValue', sender, sensor, type, payload);
+				break;
 			case C_REQ:
-			connectJeedom('getValue', sender, sensor, type, payload);
-			break;
+				connectJeedom('getValue', sender, sensor, type, payload);
+				break;
 			case C_INTERNAL:
-			switch (type) {
-				case I_BATTERY_LEVEL:
-				connectJeedom('saveBatteryLevel', sender, sensor, type, payload);
+				switch (type) {
+					case I_BATTERY_LEVEL:
+						connectJeedom('saveBatteryLevel', sender, sensor, type, payload);
+						break;
+					case I_TIME:
+						sendTime(sender, sensor, gw);
+						break;
+					case I_VERSION:
+						connectJeedom('saveLibVersion', sender, sensor, type, payload);
+						break;
+					case I_ID_REQUEST:
+						connectJeedom('getNextSensorId', sender, sensor, type, payload);
+						break;
+					case I_ID_RESPONSE:
+						break;
+					case I_INCLUSION_MODE:
+						break;
+					case I_CONFIG:
+						sendConfig(sender, gw);
+						break;
+					case I_PING:
+						break;
+					case I_PING_ACK:
+						break;
+					case I_LOG_MESSAGE:
+						break;
+					case I_CHILDREN:
+						break;
+					case I_SKETCH_NAME:
+						connectJeedom('saveSketchName', sender, sensor, type, payload);
+						break;
+					case I_SKETCH_VERSION:
+						connectJeedom('saveSketchVersion', sender, sensor, type, payload);
+						break;
+					case I_REBOOT:
+						break;
+				}
 				break;
-				case I_TIME:
-				sendTime(sender, sensor, gw);
-				break;
-				case I_VERSION:
-				connectJeedom('saveLibVersion', sender, sensor, type, payload);
-				break;
-				case I_ID_REQUEST:
-				connectJeedom('getNextSensorId', sender, sensor, type, payload);
-				break;
-				case I_ID_RESPONSE:
-				break;
-				case I_INCLUSION_MODE:
-				break;
-				case I_CONFIG:
-				sendConfig(sender, gw);
-				break;
-				case I_PING:
-				break;
-				case I_PING_ACK:
-				break;
-				case I_LOG_MESSAGE:
-				break;
-				case I_CHILDREN:
-				break;
-				case I_SKETCH_NAME:
-				connectJeedom('saveSketchName', sender, sensor, type, payload);
-				break;
-				case I_SKETCH_VERSION:
-				connectJeedom('saveSketchVersion', sender, sensor, type, payload);
-				break;
-				case I_REBOOT:
-				break;
-			}
-			break;
 			case C_STREAM:
-/*			switch (type) {
-				case ST_FIRMWARE_CONFIG_REQUEST:
-				break;
-				case ST_FIRMWARE_CONFIG_RESPONSE:
-				break;
-				case ST_FIRMWARE_REQUEST:
-				break;
-				case ST_FIRMWARE_RESPONSE:
-				break;
-				case ST_SOUND:
-				break;
-				case ST_IMAGE:
-				break;
-			}
+/*				switch (type) {
+					case ST_FIRMWARE_CONFIG_REQUEST:
+						break;
+					case ST_FIRMWARE_CONFIG_RESPONSE:
+						break;
+					case ST_FIRMWARE_REQUEST:
+						break;
+					case ST_FIRMWARE_RESPONSE:
+						break;
+					case ST_SOUND:
+						break;
+					case ST_IMAGE:
+					break;
+				}
 */			break;
 		}
 	}
 }
 
-console.log((new Date()) + " - Jeedom url : " + urlJeedom + ", gwAddress : " + gwAddress);
+logInfo("Jeedom url : " + urlJeedom + ", gwAddress : " + gwAddress);
 
 var relaunchGw = false,
     attempsGw = 0;
@@ -208,43 +232,44 @@ function launchGateway() {
   var pathsocket = '/tmp/mysensor.sock';
   fs.unlink(pathsocket, function () {
   	var server = net.createServer(function(c) {
-		console.log((new Date()) + " - Server connected");
+		logDebug("Server connected");
 		c.on('error', function(e) {
-			console.log((new Date()) + " - Error server disconnected");
+			logError("Server disconnected");
 		});
 		c.on('close', function() {
-			console.log((new Date()) + " - Connexion closed");
+			logDebug("Connexion closed");
 		});
 		c.on('data', function(data) {
-			console.log((new Date()) + " - Response: " + data);
+			logInfo("-> " + data);
 			gw.write(data.toString() + '\n');
 		});
 	});
 	server.listen(8019, function(e) {
-		console.log((new Date()) + " - server bound on 8019");
+		logInfo("- Server bound on 8019");
 	});
   });
-  console.log("Connection type " + type);
+  logInfo("Connection type " + type);
   if (type == 'serial') {
 	var SerialPort = require('serialport');
 	gw = new SerialPort(gwAddress);
-        //compatibilité avec la nouvelle verion de serialport
-        if ( gw.settings.baudRate ){
-                gw.settings.baudRate=115200;
-        }else{
-                gw.settings.baudrate=115200;
-        }
+	//compatibilité avec la nouvelle verion de serialport
+	if ( gw.settings.baudRate ){
+			gw.settings.baudRate=115200;
+	}else{
+			gw.settings.baudrate=115200;
+	}
   	gw.on('open', function() {
-  		console.log((new Date()) + " - connected to serial gateway at " + gwAddress);
+  		logInfo("- Connected to serial gateway at " + gwAddress);
   		connectJeedom('saveGateway', 0, 0, 0, 1);
-      relaunchGw = true;
-      attemptsGw = 0;
-	})
+      	relaunchGw = true;
+      	attemptsGw = 0;
+	});
 	addGatewayBehavior(gw);  
+
   } else if (type=='networkServer') {
     var tmp = gwAddress.split(':');
   	var serverGw = net.createServer(function(c) {
-		console.log((new Date()) + " - Server connected");
+		logInfo("- Server connected");
 		connectJeedom('saveGateway', 0, 0, 0, 1);
 		relaunchGw = false;
 		attemptsGw = 0;
@@ -253,60 +278,61 @@ function launchGateway() {
 		gw=c;
 	});
 	serverGw.listen(tmp[1], function(e) {
-		console.log((new Date()) + " - server bound on "+tmp[1]);
+		logInfo("- Server bound on "+tmp[1]);
 	});
 
-
-  }else {
+  } else {
     var tmp = gwAddress.split(':');
   	gw = require('net').Socket();
   	gw.connect({port: tmp[1], host: tmp[0]});
   	gw.setEncoding('ascii');
   	gw.on('connect', function() {
-  		console.log((new Date()) + " - connected to network gateway at " + gwAddress + ":" + type);
+  		logInfo("- Connected to network gateway at " + gwAddress + ":" + type);
   		connectJeedom('saveGateway', 0, 0, 0, 1);
-      relaunchGw = true;
-      attemptsGw = 0;
-  	})
+      	relaunchGw = true;
+      	attemptsGw = 0;
+  	});
 	addGatewayBehavior(gw);  
   }
+
   //common task
-	connectJeedom('saveGateway', 0, 0, 0, 0);
+  connectJeedom('saveGateway', 0, 0, 0, 0);
 }
 
 function addGatewayBehavior(gw) {
 	gw.on('data', function(rd) {
-		if (log == 'debug') {console.log((new Date()) + " : "  + rd);}
+		logDebug("received from Gateway: \"" + rd + "\"");
 		appendData(rd.toString(), gw);
 	  }).on('end', function() {
-		console.log((new Date()) + " - disconnected from gateway");
+		logInfo("- Disconnected from gateway");
 		connectJeedom('saveGateway', 0, 0, 0, 0);
 	  }).on('error', function(err) {
 		if (attempsGw < 5 && relaunchGw) {
-		console.log((new Date()) + ' Tentative de reconnexion de la gateway...');
-		setTimeout(function() {
-			detroyGw(type, gw);
-			attempsGw++;
-			launchGateway();
-			}, 5000);
+			logWarning('Tentative de reconnexion de la gateway...');
+			setTimeout(function() {
+				destroyGw(type, gw);
+				attempsGw++;
+				launchGateway();
+				}, 5000);
 		} else if (attempsGw >= 5) {
-			console.log((new Date()) + ' 5 tentatives de connexion à la gateway ('+gwAddress+') ont échouées...');
-			detroyGw(type, gw);
+			logError('5 tentatives de connexion à la gateway ('+gwAddress+') ont échouées...');
+			destroyGw(type, gw);
 		} else {
-			console.log((new Date()) + ' Error gateway: ' + err.toString());
-			console.log((new Date()) + ' ' + err.stack);
+			logError('Gateway: ' + err.toString());
+			logError(err.stack);
 		}
-		});
+	});
 }
 
-function detroyGw(type, gw) {
+function destroyGw(type, gw) {
 	if (type == 'serial') gw.close();
 	else gw.destroy();
 }
+
 launchGateway();
 
 process.on('uncaughtException', function ( err ) {
-  console.log((new Date()) + ' - An uncaughtException was found, the program will end');
-  console.log((new Date()) + ' ' + err.stack);
-	//process.exit(1);
+  logError('An uncaughtException was found, the program will end');
+  logError(err.stack);
+  //process.exit(1);
 });
